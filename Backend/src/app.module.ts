075798de642820629aee0f464d697ad938150b6c
Ángela Méndez <angelamendez@unicauca.edu.co>
2026-09-09 19:53:02 -0500

    feat: add modules for authentication, business, payments, subscriptions, and whatsapp

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { BusinessModule } from './modules/business/business.module';
+import { WhatsappModule } from './modules/whatsapp/whatsapp.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, BusinessModule, WhatsappModule, SubscriptionsModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
